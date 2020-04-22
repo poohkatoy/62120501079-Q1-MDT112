d4b02ac533c8f0f3cb4f260003200e8507cb39df
sawarin <sawarin.katoy@mail.kmutt.ac.th>
2020-04-22 22:34:38 +0700

    ThirdCommit

--- 62120501079-Q1-MDT112/62120501079-Q1-MDT112.ino
+++ 62120501079-Q1-MDT112/62120501079-Q1-MDT112.ino
@@ -1,6 +1,7 @@
 int count = 0;
 int speed = 100;
-int n = 0;
+int n = 1;
+int light = 3;
 void lightSpeed(int speed);
 void setup(){
     Serial.begin(9600); 
@@ -24,17 +25,45 @@ void setup(){
 }
 
 void loop(){
-   
-    while ( digitalRead(2) == 0 ){
+     for(light = 3 ; light <= 13 ; light++){
+        if(light == 8){
+            digitalWrite(light,0);
+            continue;
+        }
+        digitalWrite(light,1);
+        delay(speed);
+        digitalWrite(light,0);
+        int count = 0;
+        while ( digitalRead(2) == 0 ){
             count = count + 1;
-            lightSpeed(100);
+            delay(100);
             if(count<=1){
-                lightSpeed(100);
+                delay(100);
                 n = n * 2;
-                Serial.println("Faster X 2 (Current Speed : X" + String(n)")");
+                Serial.println("Faster X 2 (Current Speed : X" + String(n) + ")");
                 tone(8,400,100);
                 speed = speed / 2;
             }
+        } 
     }
-  
+    for(light = 13 ; light >= 3 ; light--){
+        if(led == 8){
+            digitalWrite(light,0);
+            continue;
+        }
+        digitalWrite(light,1);
+        delay(speed);
+        digitalWrite(light,0);
+        int count = 0;
+        while ( digitalRead(2) == 0 ){
+            count = count + 1;
+            delay(100);
+            if(press<=1){
+                delay(100);
+                Serial.println("Faster X 2 (Current Speed : X" + String(pow(2, n) + ")");
+                tone(8,400,100);
+                speed = speed / 2;
+            }
+        } 
+    }  
 }
