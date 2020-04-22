6ea59dfe773bac61abfef7c97cea9953076a39d8
sawarin <sawarin.katoy@mail.kmutt.ac.th>
2020-04-22 22:10:40 +0700

    SecCommit

--- 62120501079-Q1-MDT112/62120501079-Q1-MDT112.ino
+++ 62120501079-Q1-MDT112/62120501079-Q1-MDT112.ino
@@ -1,7 +1,40 @@
+int count = 0;
+int speed = 100;
+int n = 0;
+void lightSpeed(int speed);
 void setup(){
-
+    Serial.begin(9600); 
+    pinMode(8, OUTPUT);
+    tone(8,200,500);
+    delay(250);
+    tone(8,400,100);
+    pinMode(3, OUTPUT);
+    pinMode(4, OUTPUT);
+    pinMode(5, OUTPUT);
+    pinMode(6, OUTPUT);
+    pinMode(7, OUTPUT);
+    pinMode(8, OUTPUT);
+    pinMode(9, OUTPUT);
+    pinMode(10, OUTPUT);
+    pinMode(11, OUTPUT);
+    pinMode(12, OUTPUT);
+    pinMode(13, OUTPUT);
+    pinMode(2, INPUT_PULLUP);
+    
 }
 
 void loop(){
-    
+   
+    while ( digitalRead(2) == 0 ){
+            count = count + 1;
+            lightSpeed(100);
+            if(count<=1){
+                lightSpeed(100);
+                n = n * 2;
+                Serial.println("Faster X 2 (Current Speed : X" + String(n)")");
+                tone(8,400,100);
+                speed = speed / 2;
+            }
+    }
+  
 }
---
+++ 62120501079-Q1-MDT112/feature1.ino
@@ -0,0 +1,327 @@
+void lightSpeed(int speed){
+       
+        digitalWrite(3, 1);
+        delay(speed);
+        digitalWrite(2, 0);
+        digitalWrite(3, 0);
+        digitalWrite(4, 0);
+        digitalWrite(5, 0);
+        digitalWrite(6, 0);
+        digitalWrite(7, 0);
+        digitalWrite(8, 0);
+        digitalWrite(9, 0);
+        digitalWrite(10, 0);
+        digitalWrite(11, 0);
+        digitalWrite(12, 0);
+        digitalWrite(13, 0);
+        
+
+        digitalWrite(4, 1);
+        delay(speed);
+        digitalWrite(2, 0);
+        digitalWrite(3, 0);
+        digitalWrite(4, 0);
+        digitalWrite(5, 0);
+        digitalWrite(6, 0);
+        digitalWrite(7, 0);
+        digitalWrite(8, 0);
+        digitalWrite(9, 0);
+        digitalWrite(10, 0);
+        digitalWrite(11, 0);
+        digitalWrite(12, 0);
+        digitalWrite(13, 0);
+        
+
+        digitalWrite(5, 1);
+        delay(speed);
+        digitalWrite(2, 0);
+        digitalWrite(3, 0);
+        digitalWrite(4, 0);
+        digitalWrite(5, 0);
+        digitalWrite(6, 0);
+        digitalWrite(7, 0);
+        digitalWrite(8, 0);
+        digitalWrite(9, 0);
+        digitalWrite(10, 0);
+        digitalWrite(11, 0);
+        digitalWrite(12, 0);
+        digitalWrite(13, 0);
+        
+
+        digitalWrite(6, 1);
+        delay(speed);
+        digitalWrite(2, 0);
+        digitalWrite(3, 0);
+        digitalWrite(4, 0);
+        digitalWrite(5, 0);
+        digitalWrite(6, 0);
+        digitalWrite(7, 0);
+        digitalWrite(8, 0);
+        digitalWrite(9, 0);
+        digitalWrite(10, 0);
+        digitalWrite(11, 0);
+        digitalWrite(12, 0);
+        digitalWrite(13, 0);
+        
+
+        digitalWrite(7, 1);
+        delay(speed);
+        digitalWrite(2, 0);
+        digitalWrite(3, 0);
+        digitalWrite(4, 0);
+        digitalWrite(5, 0);
+        digitalWrite(6, 0);
+        digitalWrite(7, 0);
+        digitalWrite(8, 0);
+        digitalWrite(9, 0);
+        digitalWrite(10, 0);
+        digitalWrite(11, 0);
+        digitalWrite(12, 0);
+        digitalWrite(13, 0);
+        
+
+        digitalWrite(8, 1);
+        delay(speed);
+        digitalWrite(2, 0);
+        digitalWrite(3, 0);
+        digitalWrite(4, 0);
+        digitalWrite(5, 0);
+        digitalWrite(6, 0);
+        digitalWrite(7, 0);
+        digitalWrite(8, 0);
+        digitalWrite(9, 0);
+        digitalWrite(10, 0);
+        digitalWrite(11, 0);
+        digitalWrite(12, 0);
+        digitalWrite(13, 0);
+        
+
+        digitalWrite(9, 1);
+        delay(speed);
+        digitalWrite(2, 0);
+        digitalWrite(3, 0);
+        digitalWrite(4, 0);
+        digitalWrite(5, 0);
+        digitalWrite(6, 0);
+        digitalWrite(7, 0);
+        digitalWrite(8, 0);
+        digitalWrite(9, 0);
+        digitalWrite(10, 0);
+        digitalWrite(11, 0);
+        digitalWrite(12, 0);
+        digitalWrite(13, 0);
+        
+
+        digitalWrite(10, 1);
+        delay(speed);
+        digitalWrite(2, 0);
+        digitalWrite(3, 0);
+        digitalWrite(4, 0);
+        digitalWrite(5, 0);
+        digitalWrite(6, 0);
+        digitalWrite(7, 0);
+        digitalWrite(8, 0);
+        digitalWrite(9, 0);
+        digitalWrite(10, 0);
+        digitalWrite(11, 0);
+        digitalWrite(12, 0);
+        digitalWrite(13, 0);
+       
+
+        digitalWrite(11, 1);
+        delay(speed);
+        digitalWrite(2, 0);
+        digitalWrite(3, 0);
+        digitalWrite(4, 0);
+        digitalWrite(5, 0);
+        digitalWrite(6, 0);
+        digitalWrite(7, 0);
+        digitalWrite(8, 0);
+        digitalWrite(9, 0);
+        digitalWrite(10, 0);
+        digitalWrite(11, 0);
+        digitalWrite(12, 0);
+        digitalWrite(13, 0);
+
+
+        digitalWrite(12, 1);
+        delay(speed);
+        digitalWrite(2, 0);
+        digitalWrite(3, 0);
+        digitalWrite(4, 0);
+        digitalWrite(5, 0);
+        digitalWrite(6, 0);
+        digitalWrite(7, 0);
+        digitalWrite(8, 0);
+        digitalWrite(9, 0);
+        digitalWrite(10, 0);
+        digitalWrite(11, 0);
+        digitalWrite(12, 0);
+        digitalWrite(13, 0);
+
+
+        digitalWrite(13, 1);
+        delay(speed);
+        digitalWrite(2, 0);
+        digitalWrite(3, 0);
+        digitalWrite(4, 0);
+        digitalWrite(5, 0);
+        digitalWrite(6, 0);
+        digitalWrite(7, 0);
+        digitalWrite(8, 0);
+        digitalWrite(9, 0);
+        digitalWrite(10, 0);
+        digitalWrite(11, 0);
+        digitalWrite(12, 0);
+        digitalWrite(13, 0);
+
+        digitalWrite(12, 1);
+        delay(speed);
+        digitalWrite(2, 0);
+        digitalWrite(3, 0);
+        digitalWrite(4, 0);
+        digitalWrite(5, 0);
+        digitalWrite(6, 0);
+        digitalWrite(7, 0);
+        digitalWrite(8, 0);
+        digitalWrite(9, 0);
+        digitalWrite(10, 0);
+        digitalWrite(11, 0);
+        digitalWrite(12, 0);
+        digitalWrite(13, 0);
+
+        digitalWrite(11, 1);
+        delay(speed);
+        digitalWrite(2, 0);
+        digitalWrite(3, 0);
+        digitalWrite(4, 0);
+        digitalWrite(5, 0);
+        digitalWrite(6, 0);
+        digitalWrite(7, 0);
+        digitalWrite(8, 0);
+        digitalWrite(9, 0);
+        digitalWrite(10, 0);
+        digitalWrite(11, 0);
+        digitalWrite(12, 0);
+        digitalWrite(13, 0);
+
+        digitalWrite(10, 1);
+        delay(speed);
+        digitalWrite(2, 0);
+        digitalWrite(3, 0);
+        digitalWrite(4, 0);
+        digitalWrite(5, 0);
+        digitalWrite(6, 0);
+        digitalWrite(7, 0);
+        digitalWrite(8, 0);
+        digitalWrite(9, 0);
+        digitalWrite(10, 0);
+        digitalWrite(11, 0);
+        digitalWrite(12, 0);
+        digitalWrite(13, 0);
+
+        digitalWrite(9, 1);
+        delay(speed);
+        digitalWrite(2, 0);
+        digitalWrite(3, 0);
+        digitalWrite(4, 0);
+        digitalWrite(5, 0);
+        digitalWrite(6, 0);
+        digitalWrite(7, 0);
+        digitalWrite(8, 0);
+        digitalWrite(9, 0);
+        digitalWrite(10, 0);
+        digitalWrite(11, 0);
+        digitalWrite(12, 0);
+        digitalWrite(13, 0);
+
+        digitalWrite(8, 1);
+        delay(speed);
+        digitalWrite(2, 0);
+        digitalWrite(3, 0);
+        digitalWrite(4, 0);
+        digitalWrite(5, 0);
+        digitalWrite(6, 0);
+        digitalWrite(7, 0);
+        digitalWrite(8, 0);
+        digitalWrite(9, 0);
+        digitalWrite(10, 0);
+        digitalWrite(11, 0);
+        digitalWrite(12, 0);
+        digitalWrite(13, 0);
+
+        digitalWrite(7, 1);
+        delay(speed);
+        digitalWrite(2, 0);
+        digitalWrite(3, 0);
+        digitalWrite(4, 0);
+        digitalWrite(5, 0);
+        digitalWrite(6, 0);
+        digitalWrite(7, 0);
+        digitalWrite(8, 0);
+        digitalWrite(9, 0);
+        digitalWrite(10, 0);
+        digitalWrite(11, 0);
+        digitalWrite(12, 0);
+        digitalWrite(13, 0);
+
+        digitalWrite(6, 1);
+        delay(speed);
+        digitalWrite(2, 0);
+        digitalWrite(3, 0);
+        digitalWrite(4, 0);
+        digitalWrite(5, 0);
+        digitalWrite(6, 0);
+        digitalWrite(7, 0);
+        digitalWrite(8, 0);
+        digitalWrite(9, 0);
+        digitalWrite(10, 0);
+        digitalWrite(11, 0);
+        digitalWrite(12, 0);
+        digitalWrite(13, 0);
+
+        digitalWrite(5, 1);
+        delay(speed);
+        digitalWrite(2, 0);
+        digitalWrite(3, 0);
+        digitalWrite(4, 0);
+        digitalWrite(5, 0);
+        digitalWrite(6, 0);
+        digitalWrite(7, 0);
+        digitalWrite(8, 0);
+        digitalWrite(9, 0);
+        digitalWrite(10, 0);
+        digitalWrite(11, 0);
+        digitalWrite(12, 0);
+        digitalWrite(13, 0);
+
+        digitalWrite(4, 1);
+        delay(speed);
+        digitalWrite(2, 0);
+        digitalWrite(3, 0);
+        digitalWrite(4, 0);
+        digitalWrite(5, 0);
+        digitalWrite(6, 0);
+        digitalWrite(7, 0);
+        digitalWrite(8, 0);
+        digitalWrite(9, 0);
+        digitalWrite(10, 0);
+        digitalWrite(11, 0);
+        digitalWrite(12, 0);
+        digitalWrite(13, 0);
+
+        digitalWrite(3, 1);
+        delay(speed);
+        digitalWrite(2, 0);
+        digitalWrite(3, 0);
+        digitalWrite(4, 0);
+        digitalWrite(5, 0);
+        digitalWrite(6, 0);
+        digitalWrite(7, 0);
+        digitalWrite(8, 0);
+        digitalWrite(9, 0);
+        digitalWrite(10, 0);
+        digitalWrite(11, 0);
+        digitalWrite(12, 0);
+        digitalWrite(13, 0);
+}
